b69eea5273b4acab1b10e7992e075491647ef357
BT_2S14_14 <@>
2025-01-06 10:18:54 -0500

    TH-1: Added a print statement for when brawl causes user to lose gold.

--- src/Town.java
+++ src/Town.java
@@ -124,6 +124,9 @@ public class Town
                 printMessage += "That'll teach you to go lookin' fer trouble in MY town! Now pay up!";
                 printMessage += "\nYou lost the brawl and pay " +  goldDiff + " gold.";
                 hunter.changeGold(-1 * goldDiff);
+                if (hunter.getGold()==0){
+                    System.out.println("You have lost all your gold. Game over!");
+                }
             }
         }
     }
